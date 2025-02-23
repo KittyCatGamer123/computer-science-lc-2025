1534cc60836e67de177ffa198888c6af4a3e7e9d
catster <mirololeo@gmail.com>
2025-02-23 22:29:37 +0000

    Inital Commit

---
+++ Report/main.js
@@ -0,0 +1,43 @@
+/*
+    Runs all needed functions when the Body's HTML has loaded.
+    Does not return anything.
+*/
+function Initialise() {
+    WordCountSum();
+    CreateCollapsibles();
+}
+
+/*
+    Gets all of the word counts from each section
+    of the word count table, and sets it as the total value.
+    Does not return anything.
+*/
+function WordCountSum() {
+    let Sum = 0;
+    const TableItems = document.getElementById("WordCounts").children[0];
+
+    // Start at 1 to skip the header items
+    // Skip the sum items
+    for (let i = 1; i < TableItems.children.length - 1; i++) {
+        let items = TableItems.children[i]
+        Sum += parseInt(items.children[1].textContent);
+    }
+
+    document.getElementById("WordCountSum").innerHTML = `<b>${Sum.toString()}</b>`;
+}
+
+/*
+    Gets all elements of the "collapsible" class, and adds Event Listeners to them
+    so their content can be toggled on and off.
+    Does not return anything.
+*/
+function CreateCollapsibles() {
+    let Colls = document.getElementsByClassName("collapsible");
+    
+    for (let i = 0; i < Colls.length; i++) {
+        Colls[i].addEventListener("click", function() {
+            let content = this.nextElementSibling;
+            content.style.display = (content.style.display === "block") ? "none" : "block";
+        })
+    }
+}
